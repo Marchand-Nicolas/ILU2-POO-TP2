4fccbe33a606822e5a7a092e4544c10455aa0f0e
Nico <60229704+Marchand-Nicolas@users.noreply.github.com>
2026-03-31 13:35:43 +0200

    fin tp

--- src/controleur/ControlAcheterProduit.java
+++ src/controleur/ControlAcheterProduit.java
@@ -1,5 +1,7 @@
 package controleur;
 
+import personnages.Gaulois;
+import villagegaulois.Etal;
 import villagegaulois.Village;
 
 public class ControlAcheterProduit {
@@ -15,5 +17,53 @@ public class ControlAcheterProduit {
 		this.controlTrouverEtalVendeur = controlTrouverEtalVendeur;
 	}
 
-	//TODO a completer
+	public Etal[] listerEtalsVendantProduit(String nomProduit) {
+		Gaulois[] vendeurs = village.rechercherVendeursProduit(nomProduit);
+		if (vendeurs == null) 
+			return new Etal[0];
+		Etal[] etals = new Etal[vendeurs.length];
+		for (int i = 0; i < vendeurs.length; i++) {
+			Gaulois gaulois = vendeurs[i];
+			Etal etal = controlTrouverEtalVendeur.trouverEtalVendeur(gaulois.getNom());
+			etals[i] = etal;
+		}
+		return etals;
+	}
+	
+	public Gaulois[] listerVendeursProduit(String nomProduit) {
+		Etal[] etals = listerEtalsVendantProduit(nomProduit);
+		Gaulois[] res = new Gaulois[etals.length];
+		for (int i = 0; i < etals.length; i++) {
+			Etal etal = etals[i];
+			res[i] = etal.getVendeur();
+		}
+		return res;
+	}
+	
+	public String afficherVendeursProduit(String nomProduit) {
+		Gaulois[] vendeurs = listerVendeursProduit(nomProduit);
+		StringBuilder res = new StringBuilder();
+		for (int i = 0; i < vendeurs.length; i++) {
+			Gaulois vendeur = vendeurs[i];
+			res.append(" - ");
+			res.append(i + 1);
+			res.append(" ");
+			res.append(vendeur.getNom());
+			res.append("\n");
+		}
+		return res.toString();
+	}
+	
+	public int acheterProduit(Gaulois vendeur, int quantite) {
+		Etal etal = controlTrouverEtalVendeur.trouverEtalVendeur(vendeur.getNom());
+		return etal.acheterProduit(quantite);
+	}
+	
+	public boolean estProduitVendu(String nomProduit) {
+		return listerEtalsVendantProduit(nomProduit).length > 0;
+	}
+	
+	public boolean estHabitant(String nomGaulois) {
+		return controlVerifierIdentite.verifierIdentite(nomGaulois);
+	}
 }
--- src/frontiere/BoundaryAcheterProduit.java
+++ src/frontiere/BoundaryAcheterProduit.java
@@ -1,6 +1,7 @@
 package frontiere;
 
 import controleur.ControlAcheterProduit;
+import personnages.Gaulois;
 
 public class BoundaryAcheterProduit {
 	private ControlAcheterProduit controlAcheterProduit;
@@ -10,6 +11,52 @@ public class BoundaryAcheterProduit {
 	}
 
 	public void acheterProduit(String nomAcheteur) {
-		// TODO à completer
+		if (!controlAcheterProduit.estHabitant(nomAcheteur)) {
+			System.out.println("Je suis désolée "+ nomAcheteur +" mais il faut être un habitant de notre village pour commercer ici.");
+			return;
+		}
+		String nomProduit = null;
+		do {
+			nomProduit = Clavier.entrerChaine("Quel produit voulez-vous acheter?\n");
+			if (!controlAcheterProduit.estProduitVendu(nomProduit)) {
+				System.out.println("Désolé, personne ne vend ce produit au marché.\n");
+			}
+		} while (nomProduit == null || !controlAcheterProduit.estProduitVendu(nomProduit));
+		int numVendeur = 0;
+		Gaulois[] vendeurs = controlAcheterProduit.listerVendeursProduit(nomProduit);
+		do {
+			numVendeur = Clavier.entrerEntier("Chez quel commerçant voulez-vous acheter des fleurs ?\n" + controlAcheterProduit.afficherVendeursProduit(nomProduit));
+			if (0 >= numVendeur || numVendeur > vendeurs.length) {
+				System.out.println("Ce vendeur n'existe pas.");
+			}
+		} while (0 >= numVendeur || numVendeur > vendeurs.length);
+		Gaulois vendeur = vendeurs[numVendeur - 1];
+		System.out.println(nomAcheteur + " se déplace jusqu'à l'étal du vendeur " + vendeur.getNom() + ".\n");
+		int nbProduits = -1;
+		do {
+			nbProduits = Clavier.entrerEntier("Bonjour " + nomAcheteur + ", combien de " + nomProduit + " voulez-vous acheter ?\n");
+			if (nbProduits < 0) {
+				System.out.println("Quantité invalide!\n");
+			}
+		} while (0 > nbProduits);
+		int quantiteFinale = controlAcheterProduit.acheterProduit(vendeur, nbProduits);
+		if (quantiteFinale == nbProduits)
+			System.out.println(nomAcheteur + " achète " + nbProduits + " " + nomProduit + " à " + vendeur.getNom());
+		else if (quantiteFinale == 0) 
+			System.out.println(nomAcheteur + " veut acheter " + nbProduits + " "
+					+ nomProduit
+					+ " malheureusement il n’y en a plus !");
+			else System.out.println(nomAcheteur + " veut acheter "
+					+ nbProduits + " "
+					+ nomProduit
+					+ " malheureusement "
+					+ vendeur.getNom()
+					+ " n’en a plus que "
+					+ quantiteFinale
+					+ ". "
+					+ nomAcheteur
+					+ " achète tout le stock de "
+					+ vendeur.getNom() + ".");
+		
 	}
 }
