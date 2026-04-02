cda56d707d38096c83e3071d92611eaf61f5a247
Nico <60229704+Marchand-Nicolas@users.noreply.github.com>
2026-04-02 17:37:05 +0200

    moitie test controlacheterproduit

---
+++ src/test/ControlAcheterProduitTest.java
@@ -0,0 +1,52 @@
+package test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import controleur.ControlAcheterProduit;
+import controleur.ControlPrendreEtal;
+import controleur.ControlTrouverEtalVendeur;
+import controleur.ControlVerifierIdentite;
+import personnages.Chef;
+import villagegaulois.Village;
+
+class ControlAcheterProduitTest {
+	private ControlAcheterProduit controlAcheterProduit;
+	private ControlPrendreEtal controlPrendreEtal;
+	private Village village;
+	private Chef abra;
+
+	@BeforeEach
+	public void initialiserSituation() {
+		System.out.println("Initialisation...");
+		village = new Village("Le village des irréductibles", 10, 5);
+		ControlVerifierIdentite controlVerifierIdentite = new ControlVerifierIdentite(village);
+		ControlTrouverEtalVendeur controlTrouverEtalVendeur = new ControlTrouverEtalVendeur(village);
+		controlAcheterProduit = new ControlAcheterProduit(controlVerifierIdentite, controlTrouverEtalVendeur, village);
+		abra = new Chef("abra", 10, village);
+		village.setChef(abra);
+		controlPrendreEtal = new ControlPrendreEtal(controlVerifierIdentite, village);
+	}
+	
+	@Test
+	public void testEstHabitant() {
+		assertEquals(false, controlAcheterProduit.estHabitant("zzz"));
+		assertEquals(true, controlAcheterProduit.estHabitant("abra"));
+	}
+	
+	@Test
+	public void testEstProduitVendu() {
+		assertEquals(false, controlAcheterProduit.estProduitVendu("pomme"));
+		controlPrendreEtal.prendreEtal("abra", "pomme", 5);
+		assertEquals(true, controlAcheterProduit.estProduitVendu("pomme"));
+	}
+	
+	@Test
+	public void testAcheterProduit() {
+		controlPrendreEtal.prendreEtal("abra", "pomme", 5);
+		assertEquals(3, controlAcheterProduit.acheterProduit(abra, 3));
+		assertEquals(2, controlAcheterProduit.acheterProduit(abra, 3));
+	}
+}
