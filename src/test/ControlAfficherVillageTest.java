1b7ebf7667dd91044efd22a13fdfd92d6a3120cd
Nico <60229704+Marchand-Nicolas@users.noreply.github.com>
2026-04-02 16:49:52 +0200

    test verifid, affichervillage

--- src/test/ControlAfficherVillageTest.java
+++ src/test/ControlAfficherVillageTest.java
@@ -2,13 +2,44 @@ package test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import controleur.ControlAfficherVillage;
+import controleur.ControlEmmenager;
+import personnages.Chef;
+import villagegaulois.Village;
+
 class ControlAfficherVillageTest {
+	private ControlAfficherVillage controlAfficherVillage;
+	private Village village;
 
-	@Test
-	void test() {
-		fail("Not yet implemented");
+	@BeforeEach
+	public void initialiserSituation() {
+		System.out.println("Initialisation...");
+		village = new Village("Le village des irréductibles", 10, 5);
+		controlAfficherVillage = new ControlAfficherVillage(village);
+		Chef abra = new Chef("abra", 10, village);
+		village.setChef(abra);
 	}
-
+	
+	@Test
+	public void testDonnerNomVillage() {
+		assertEquals("Le village des irréductibles", controlAfficherVillage.donnerNomVillage());
+	}
+	
+	@Test
+	public void testDonnerNomsVillageois() {
+		String[] expected1 = { "abra" };
+		assertArrayEquals(expected1, controlAfficherVillage.donnerNomsVillageois());
+		ControlEmmenager controlEmmenager = new ControlEmmenager(village);
+		controlEmmenager.ajouterGaulois("john", 1);
+		String[] expected2 = { "abra", "john" };
+		assertArrayEquals(expected2, controlAfficherVillage.donnerNomsVillageois());
+	}
+	
+	@Test
+	public void donnerNbEtals() {
+		assertEquals(5, controlAfficherVillage.donnerNbEtals());
+	}
 }
---
+++ src/test/ControlVerifierIdentiteTest.java
@@ -0,0 +1,31 @@
+package test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import controleur.ControlVerifierIdentite;
+import personnages.Chef;
+import villagegaulois.Village;
+
+class ControlVerifierIdentiteTest {
+	private Village village;
+	private Chef abra;
+	
+	@BeforeEach
+	public void initialiserSituation() {
+		System.out.println("Initialisation...");
+		village = new Village("Le village des irréductibles", 10, 5);
+		abra = new Chef("abra", 10, village);
+		village.setChef(abra);
+	}
+	
+	@Test
+	public void testVerifierIdentite() {
+		ControlVerifierIdentite controlVerifierIdentite = new ControlVerifierIdentite(village);
+		assertEquals(true, controlVerifierIdentite.verifierIdentite("abra"));
+		assertNotEquals(true, controlVerifierIdentite.verifierIdentite("jeff"));
+	}
+
+}
